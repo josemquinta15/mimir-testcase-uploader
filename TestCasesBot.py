b680e1bf99b605bc26a688f76872fd6c53d41951
josemquinta <josemiguelquinta@uc.cl>
2021-04-10 00:29:27 -0400

    Fixed bugs, and homework selector

--- TestCasesBot.py
+++ TestCasesBot.py
@@ -4,14 +4,13 @@ from selenium.webdriver.common.keys import Keys
 from getpass import getpass
 import time
 
+from parameters import EDIT_BUT_XPATH, visibility_options_id
+import parameters as p
+
 COURSE_LINK = 'https://class.mimir.io/courses/4f1e9d3e-57b4-4a0e-9e5e-3091fd35f5b2'
 autogenerate_output_time = 3
 
-ALL_COURSE_XPATH = '/html/body/div[3]/div/div[2]/div[3]/div[2]/div/div/div[2]/div/div/div[1]/div/div[1]/div[2]'
-EDIT_BUT_XPATH = '/html/body/div[3]/div/div[2]/div[3]/div[2]/div/div/div[1]/div[1]/div[1]/div[2]/div[2]/div/a[1]'
-visibility_options_id = ['TestCaseForm--showInputCheckbox-label', 'TestCaseForm--showSubmissionOutputCheckbox-label', \
-    'TestCaseForm--showCompilerOutputCheckbox-label', 'TestCaseForm--showSubmissionDiffCheckbox-label',\
-        'TestCaseForm--showExpectedOutputCheckbox-label', 'TestCaseForm--showErrorSummariesCheckbox-label']
+
 
 
 class Bot:
@@ -32,24 +31,51 @@ class Bot:
         self.driver.get(COURSE_LINK)
         time.sleep(1)
 
-    def select_assignment(self, assignment_name):
-        # assignments = self.driver.find_elements_by_xpath(ALL_COURSE_XPATH)
-        try:
-            i = 1
-            ready = False
-            while not ready:
-                xpath = ALL_COURSE_XPATH + f'/div[{i}]/div[2]/div/div/a'
-                if self.driver.find_element_by_xpath(xpath).text == assignment_name:
-                    print(f'Entering: {self.driver.find_element_by_xpath(xpath).text}')
-                    ready = True
-                    self.driver.find_element_by_xpath(xpath).click()
-                i += 1
+    def get_assignments(self):
+        assignments = []
+        assignments.append(self.driver.find_elements_by_xpath(p.DRAFT_COURSE_XPATH))
+        assignments.append(self.driver.find_elements_by_xpath(p.SCHEDULED_COURSE_XPATH))
+        assignments.append(self.driver.find_elements_by_xpath(p.LIVE_COURSE_XPATH))
 
-        except NoSuchElementException as e:
-           print(e)
+        return assignments
+
+    def select_assignment(self, assignment_name):
+        assignments = self.get_assignments()
+
+        paths = [p.DRAFT_COURSE_XPATH, p.SCHEDULED_COURSE_XPATH, p.LIVE_COURSE_XPATH]
+
+        for path in paths:
+            for i in range(p.MAX_TASKS_PER_COURSEWORK):
+                try:
+                    xpath = path + f'/div[{i}]/div[2]/div/div/a'
+                    if self.driver.find_element_by_xpath(xpath).text == assignment_name:
+                        print(f'Entering: {self.driver.find_element_by_xpath(xpath).text}')
+                        self.driver.find_element_by_xpath(xpath).click()
+                
+                except NoSuchElementException as e:
+                    pass
+                
+
+
         
-        finally:
-            time.sleep(1)
+        # found = False
+        # while not found:
+        #     try:
+        #         i = 1
+        #         ready = False
+        #         while not ready:
+        #             xpath = ALL_COURSE_XPATH + f'/div[{i}]/div[2]/div/div/a'
+        #             if self.driver.find_element_by_xpath(xpath).text == assignment_name:
+        #                 print(f'Entering: {self.driver.find_element_by_xpath(xpath).text}')
+        #                 ready = True
+        #             self.driver.find_element_by_xpath(xpath).click()
+        #             i += 1
+        #         found = True 
+        #         continue   
+        #     except NoSuchElementException as e:
+        #         print(e)
+            
+        # time.sleep(4)
 
     def select_part(self, part_name, base_xpath):
         try:
@@ -63,15 +89,16 @@ class Bot:
                 i += 1
 
         except NoSuchElementException as e:
-           print(e)
-        
-        finally:
-            time.sleep(1)
+          time.sleep(0.5)
 
     def enter_edit_menu(self):
-        edit_but = self.driver.find_element_by_id('AssignmentOverview--editAssignment')
-        edit_but.click()
-        time.sleep(2)
+        while True:
+            try:
+                edit_but = self.driver.find_element_by_id('AssignmentOverview--editAssignment')
+                edit_but.click()
+                break
+            except NoSuchElementException:
+                time.sleep(0.5)
 
     def send_data_by_id(self, element_id, data_to_send, sleep_time = 0.5):
         element = self.driver.find_element_by_id(element_id)
@@ -88,7 +115,11 @@ class Bot:
         command_line_args = '--fail-on-nonzero-exit', public = True):
 
         # Select part of the homework to add testcase
-        base_xpath = self.select_part(question_name, '//*[@id="AssignmentForm--editQuestionsContainer"]')        
+        base_xpath = None
+        while not base_xpath:
+            base_xpath = self.select_part(question_name, '//*[@id="AssignmentForm--editQuestionsContainer"]')        
+            # time.sleep(1)
+            
         
         # Add new testcase to the preselected homework part
         open_menu = False
@@ -102,45 +133,88 @@ class Bot:
                 time.sleep(2)
                 open_menu = True
             except ElementNotInteractableException:
-                continue
+                time.sleep(0.5)
 
         # Settings of the test case
-        self.send_data_by_id('TestCaseForm--nameInput', name)
-        self.send_data_by_id('TestCaseForm--pointsInput', points)
+        passed = False
+        while not passed:
+            try:
+                self.send_data_by_id('TestCaseForm--nameInput', name)
+                self.send_data_by_id('TestCaseForm--pointsInput', points)
+                passed = True
+            except:
+                time.sleep(0.5)
 
         #Set I/O testcase
-        test_case_type = self.driver.find_element_by_xpath('//*[@id="TestCaseForm--testTypeSelect"]/option[2]').click()
-        time.sleep(1)
+        passed = False
+        while not passed:
+            try:
+                test_case_type = self.driver.find_element_by_xpath('//*[@id="TestCaseForm--testTypeSelect"]/option[2]').click()
+                passed = True
+            except:
+                time.sleep(0.5)
 
         #Set Description
-        self.send_data_by_id('TestCaseForm--descriptionInput', description)
+        # self.send_data_by_id('TestCaseForm--descriptionInput', description)
 
         #Set input of testcase
-        input_space = self.driver.find_element_by_xpath('//*[@id="TestCaseForm--inputEditor"]/textarea')
-        input_space.send_keys(Keys.BACK_SPACE)
-        input_space.send_keys(input_code)
+        passed = False
+        while not passed:
+            try:
+                input_space = self.driver.find_element_by_xpath('//*[@id="TestCaseForm--inputEditor"]/textarea')
+                input_space.send_keys(Keys.BACK_SPACE)
+                input_space.send_keys(input_code)
+                passed = True
+            except:
+                time.sleep(0.5)
 
         #Generate output of testcase
-        self.click_by_id('TestCaseForm--autoGenerateOutputButton', autogenerate_output_time)
+        while True:
+            try:
+                self.click_by_id('TestCaseForm--autoGenerateOutputButton', autogenerate_output_time)
+                break
+            except:
+                time.sleep(0.5)
+        # time.sleep(2)
 
         # Set strict whitespace
-        if strict_whitespace:
-            self.click_by_id('TestCaseForm--strictWhitespaceCheckbox-label', 0)
+        while True:
+            try:
+                if strict_whitespace:
+                    self.click_by_id('TestCaseForm--strictWhitespaceCheckbox-label', 0)
+                break
+            except:
+                time.sleep(0.5)
 
         # Set success threshold and command line arguments
-        self.send_data_by_id('TestCaseForm--thresholdInput', success_threshold)
-        self.send_data_by_id('TestCaseForm--executionClargsInput', command_line_args)
+        while True:
+            try:
+                self.send_data_by_id('TestCaseForm--thresholdInput', success_threshold)
+                self.send_data_by_id('TestCaseForm--executionClargsInput', command_line_args)
+                break
+            except:
+                time.sleep(0.5)
 
         # Set private or public
-        if not public:
-            for id in visibility_options_id:
-                self.click_by_id(id, 0)
-        time.sleep(1)
+        while True:
+            try:
+                if not public:
+                    for id in visibility_options_id:
+                        self.click_by_id(id, 0)
+                break
+            
+            except:
+                time.sleep(0.5)
 
         # Save and exit
-        self.click_by_id('TestCaseForm--saveButton', 1)
-        self.click_by_id('TestCaseForm--exitWithoutSaving', 2)
-        print(f'Added Correctly {name} testcase')
+        while True:
+            try:
+                self.click_by_id('TestCaseForm--saveButton', 1)
+                self.click_by_id('TestCaseForm--exitWithoutSaving', 2)
+                print(f'Added Correctly {name} testcase')
+                break
+            except:
+                time.sleep(0.5)
         
         
 
@@ -155,8 +229,8 @@ if __name__ == '__main__':
     bot.enter_course()
     bot.select_assignment('Tarea 1')
     bot.enter_edit_menu()
-    bot.add_testcase('Auto generated Private Testcase', 5, 'description', 'input code\npasswd\npasswd', 'Parte 1 - Conexión Wi-Fi', public = False)
-    bot.add_testcase('Auto generated Public Testcase', 6, 'description', 'network\npass1\npass2', 'Parte 2 - Prueba')
+    bot.add_testcase('Auto generated Private Testcase', 5, 'description', 'input code\npasswd\npasswd', 'Parte 5 - Conexión a Wi-Fi', public = False)
+    bot.add_testcase('Auto generated Public Testcase', 6, 'description', 'network\npass1\npass2', 'Parte 5 - Conexión a Wi-Fi')
     
 
     
---
+++ parameters.py
@@ -0,0 +1,11 @@
+DRAFT_COURSE_XPATH = '/html/body/div[3]/div/div[2]/div[3]/div[2]/div/div/div[2]/div/div/div[1]/div/div[1]/div[2]'
+SCHEDULED_COURSE_XPATH = '/html/body/div[3]/div/div[2]/div[3]/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div[2]'
+LIVE_COURSE_XPATH = '/html/body/div[3]/div/div[2]/div[3]/div[2]/div/div/div[2]/div/div/div[1]/div/div[3]/div[2]'
+                    
+
+EDIT_BUT_XPATH = '/html/body/div[3]/div/div[2]/div[3]/div[2]/div/div/div[1]/div[1]/div[1]/div[2]/div[2]/div/a[1]'
+visibility_options_id = ['TestCaseForm--showInputCheckbox-label', 'TestCaseForm--showSubmissionOutputCheckbox-label', \
+    'TestCaseForm--showCompilerOutputCheckbox-label', 'TestCaseForm--showSubmissionDiffCheckbox-label',\
+        'TestCaseForm--showExpectedOutputCheckbox-label', 'TestCaseForm--showErrorSummariesCheckbox-label']
+
+MAX_TASKS_PER_COURSEWORK = 50
